1874526112a2a6243fceedb11f984d074ea9a1a7
asmi046 <asmi046@gmail.com>
2024-08-27 15:38:23 +0300

    Лого

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.all')
 
 @php
-    $title = "";
-    $description = "";
+    $title = "Адвокат Дмитрий Горяинов";
+    $description = "Адвокат Дмитрий Горяинов - правовая защита грвждан";
 @endphp
 
 @section('title', $title)
 @section('description', $description)
 
 @section('main')
+
+<div class="zagl_wrapper">
+    <img src="{{ asset('img/logo.webp') }}" alt="{{ $title }}">
+</div>
 
 @endsection
 
